59dc8c809198eb38032b582da8565f83558d75fc
eprim7 <evan.primasing7@gmail.com>
2025-01-29 13:49:15 -0600

    testing vercel to see if it updates

--- tavern_keeper/src/pages/Home/Home.jsx
+++ tavern_keeper/src/pages/Home/Home.jsx
@@ -5,9 +5,7 @@ function Home() {
     return (
         <div className={styles.container}>
             <h1 className={styles.header}>Tavern Keeper</h1>
-            <div className={styles.background}>
-                <img src="/assets/home.png" alt="Cool home" className={styles.pic}/>
-            </div>
+            <h2>Testing to see if it pushes to vercel</h2>
         </div>
     );
 }
