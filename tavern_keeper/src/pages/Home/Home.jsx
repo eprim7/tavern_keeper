28b0b25543896c2d7885f1158611599881fe18ad
eprim7 <evan.primasing7@gmail.com>
2025-02-06 16:10:26 -0600

    updated the home page and added responsiveness

--- tavern_keeper/src/pages/Home/Home.jsx
+++ tavern_keeper/src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
             <div className={styles.content}>
                 <div className={styles.character}>
                     <img src="/assets/homeCharacters.png" alt="characters" className={styles.characterPic}/>
-                    <p className={styles.p}>Bring your characters to life by customizing them directly from your novel, book, or D&D game.</p>
+                    <p className={styles.p}>Bring your characters to life by customizing them directly from your novel, book, or D&D game!</p>
                 </div>
                 <div className={styles.character}>
                     <p className={styles.p}>Effortlessly upload, store, and edit customized maps of your worlds.</p>
