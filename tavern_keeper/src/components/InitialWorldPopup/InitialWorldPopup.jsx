065282709bf9b02b41cc2ae047648f1f8322aa45
eprim7 <evan.primasing7@gmail.com>
2025-02-10 19:55:17 -0600

    began the world page, and implemented the popup for the world page

---
+++ tavern_keeper/src/components/InitialWorldPopup/InitialWorldPopup.jsx
@@ -0,0 +1,45 @@
+import styles from './InitialWorldPopup.module.css';
+
+const InitialWorldPopup = ({ closeModal}) => {
+    return(
+
+        <>
+            <div className={styles.modalBackground}>
+                <div className={styles.modalContainer}>
+                    <div className={styles.titleCloseBtn}>
+                        <button onClick={() => closeModal(false)}> X </button>
+                    </div>
+                    <div className={styles.title}>
+                        <h1 className={styles.h1}>Create Your World</h1>
+                        <hr />
+                    </div>
+                    <div className={styles.body}>
+                        <label htmlFor="worldName" className={styles.label}>World Name</label>
+                        <input type="text" placeholder='world name' className={styles.nameInput} id='worldName' name='worldName' />
+                    </div>
+                    <div className={styles.body}>
+                        <label htmlFor="worldPicture" className={styles.label}>World Picture</label>
+                        <input type="file" className={styles.pictureInput} accept='image/png, image/jpeg' id='worldPicture' name='worldPicture' />
+                    </div>
+                    <div className={styles.body}>
+                        <label htmlFor="worldGenre" className={styles.label}>World Genre</label>
+                        <select name="worldGenre" id="worldGenre" className={styles.worldGenre}>
+                         <option value="Select" disabled hidden selected>Select an Option</option>
+                            <option value="Sci-Fi">Sci-Fi</option>
+                            <option value="darkFantasy">Dark Fantasy</option>
+                            <option value="Comedy">Comedy</option>
+                            <option value="Horror">Horror</option>
+                        </select>
+                    </div>
+                    <div className={styles.footer}>
+                        <button onClick={() => closeModal(false)} className={styles.cancelBtn}>Cancel</button>
+                        <button>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}   
+
+
+export default InitialWorldPopup
--- tavern_keeper/src/pages/World/World.jsx
+++ tavern_keeper/src/pages/World/World.jsx
@@ -1,15 +1,23 @@
 import Header from "../../components/Header/Header";
 import styles from "../World/World.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import InitialWorldPopup from "../../components/InitialWorldPopup/InitialWorldPopup";
+
 function World() {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
     return (
         <>
             <Header />
-
             <div className={styles.hero}>
-                <div className={styles.card}>
-                    <h3 className={styles.h3}><Link className={styles.link}>Click here to start your world making journey</Link></h3>
+                <div className={styles.card} >
+                    <h3 className={styles.h3} onClick={() => setIsPopupOpen(true)}>
+                        <Link className={styles.link}>Click here to start your world-making journey</Link>
+                    </h3>
                 </div>
+                {isPopupOpen && <InitialWorldPopup closeModal={setIsPopupOpen}/>}
+
             </div>
         </>
     );
