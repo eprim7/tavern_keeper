c94cbe2d29e703c0d967654824ca1f4517e13d66
eprim7 <evan.primasing7@gmail.com>
2025-03-27 11:22:29 -0500

    created a grid for the popup messages from the subpages

--- tavern_keeper/src/subpages/Miscellaneous/Miscellaneous.jsx
+++ tavern_keeper/src/subpages/Miscellaneous/Miscellaneous.jsx
@@ -2,43 +2,47 @@ import Header from "../../components/Header/Header"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import styles from "../Characters/Characters.module.css"
 import WorldOverviewGrid from "../../components/WorldOverviewGrid/WorldOverviewGrid"
-import { useNavigate } from "react-router-dom";
+import { useState } from "react"
+import SubpagesPopup from "../../components/SubpagesPopup/SubpagesPopup"
 
-function Miscellaneous(){
-    const navigate = useNavigate();
-
-    const loadPage = () => {
-        navigate("/mapPopup");
-    };
-
-
-
-    return(
-        
-        <>
-            <Header />
+function Miscellaneous() {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+  
+    return (
+      <>
+        <Header />
+        <div className={styles.container}>
+          <div className={styles.sidebar}>
+            <Sidebar />
+          </div>
+  
+          <div className={styles.content}>
+            <button className={styles.button} onClick={() => setIsPopupOpen(true)}>
+              Add new Miscellaneous
+            </button>
             
-            <div className={styles.container}>
-                <div className={styles.sidebar}>
-                    <Sidebar />
-                </div>
-
-                <div className={styles.content}>
-                    <button className={styles.button} onClick={loadPage}>Add new Miscellaneous</button>
-
-                    <WorldOverviewGrid>
-                        <div>Name of Miscellaneous</div>
-                        <div>Name of Miscellaneous</div>
-                        <div>Name of Miscellaneous</div>
-                        <div>Name of Miscellaneous</div>
-                        <div>Name of Miscellaneous</div>
-                        <div>Name of Miscellaneous</div>
-                    </WorldOverviewGrid>
-                </div>
-            </div>
-
-        </>
-    )
-}
-
-export default Miscellaneous
+            {isPopupOpen && (
+              <SubpagesPopup
+                closeModal={setIsPopupOpen}
+                showPicture={false} // Set to false to hide the picture input
+              >
+                Miscellaneous
+              </SubpagesPopup>
+            )}
+  
+            <WorldOverviewGrid>
+              <div>Name of Miscellaneous</div>
+              <div>Name of Miscellaneous</div>
+              <div>Name of Miscellaneous</div>
+              <div>Name of Miscellaneous</div>
+              <div>Name of Miscellaneous</div>
+              <div>Name of Miscellaneous</div>
+            </WorldOverviewGrid>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Miscellaneous;
+  
