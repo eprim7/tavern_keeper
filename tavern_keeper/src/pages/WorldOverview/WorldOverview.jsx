ffdffd6a376a4d39acc8b260a8ca3838d48d336e
eprim7 <evan.primasing7@gmail.com>
2025-04-22 15:38:31 -0500

    added responsiveness to the worldOverview page

--- tavern_keeper/src/pages/WorldOverview/WorldOverview.jsx
+++ tavern_keeper/src/pages/WorldOverview/WorldOverview.jsx
@@ -1,74 +1,70 @@
 import { useParams } from "react-router-dom";
-import Header from "../../components/Header/Header"
-import Sidebar from "../../components/Sidebar/Sidebar"
-import styles from '../WorldOverview/WorldOverview.module.css';
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import styles from "../WorldOverview/WorldOverview.module.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import supabase from "../../api/supabase-client";
 
-function WorldOverview(){
-    const [worldName, setWorldName] = useState('')
-    const [worldDescription, setWorldDescription] = useState('')
+function WorldOverview() {
+  const [worldName, setWorldName] = useState("");
+  const [worldDescription, setWorldDescription] = useState("");
 
-    const { id } = useParams()
-    let email = localStorage.getItem('email')
+  const { id } = useParams();
+  let email = localStorage.getItem("email");
 
-    // gets everything from the email before the @
-    let username = email?.split('@')[0];
-    
-    // ensures user is signed in
-    const navigate = useNavigate();
-    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+  // gets everything from the email before the @
+  let username = email?.split("@")[0];
 
-    useEffect(() => {
-        if (!isLoggedIn) {
-        navigate("/signin"); // redirect to signin if not logged in
-        } // end of if
-        else {
-            fetchWorlds()
-        } // end of else
-    }, [isLoggedIn, navigate]);
+  // ensures user is signed in
+  const navigate = useNavigate();
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
 
-    const fetchWorlds = async () =>{
-        const {data: userData, error: userError} = await supabase
-        .from("Worlds")
-        .select(`title, description`)
-        .eq("id", id)
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/signin"); // redirect to signin if not logged in
+    } else {
+      fetchWorlds();
+    }
+  }, [isLoggedIn, navigate]);
 
-        console.log("fetching user data",userData)
+  const fetchWorlds = async () => {
+    const { data: userData, error: userError } = await supabase
+      .from("Worlds")
+      .select(`title, description`)
+      .eq("id", id);
 
-        setWorldName(userData[0].title)
-        setWorldDescription(userData[0].description)
-    } // end of fetchWorlds
+    if (userData && userData.length > 0) {
+      setWorldName(userData[0].title);
+      setWorldDescription(userData[0].description);
+    }
+  };
 
+  return (
+    <>
+      <Header />
+      <h1 className={styles.title}>Welcome {username}</h1>
 
-    return(
+      <div className={styles.pageWrapper}>
+            <div className={styles.sidebar}>
+                <Sidebar worldID={id} />
+            </div>
 
-        <>
-        <Header />
-        <h1 className={styles.title}>Welcome {username}</h1>
-
-        <div className={styles.sidebar}>
-            <Sidebar  worldID={id}/>
-        </div>
-
-        <div className={styles.pageWrapper}>
-            <div className={styles.middleContainer}>
+            <div className={styles.worldInfoContainer}>
                 <div className={styles.infoRow}>
-                    <h1>World Name:</h1>
+                    <h1 className={styles.label}>World Name:</h1>
                     <h1 className={styles.h1}>{worldName}</h1>
                 </div>
 
                 <div className={styles.infoRow}>
-                    <h1>World Description:</h1>
+                    <h1 className={styles.label}>World Description:</h1>
                     <h1 className={styles.h1}>{worldDescription}</h1>
                 </div>
             </div>
         </div>
 
-        </>
-    )
+    </>
+  );
 }
 
-
-export default WorldOverview
+export default WorldOverview;
